12e4b4989e409970dceb2f7bb671aa5fa0f50a07
Nadeef Muttaqin Chowdhury <nadeefchowdhury@gmail.com>
2026-06-11 22:00:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 # Initialize model
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 st.set_page_config(page_title="Gemini Chatbot", layout="centered")
 st.title("💬 Gemini Chatbot")
